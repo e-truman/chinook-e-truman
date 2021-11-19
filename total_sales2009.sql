62e2f0855f4ffdfa8cac220ec4926319916a8de9
Erin <etruman2@gmail.com>
2021-11-19 15:03:40 -0600

    total sales

--- total_sales2009.sql
+++ total_sales2009.sql
@@ -1,2 +1,6 @@
 -- What are the respective total sales for each of those years?
 -- HINT: SUM
+
+SELECT SUM(Total) as Total_Sales_2009
+FROM Invoice
+WHERE InvoiceDate Like "%2009-%";
--- total_sales2011.sql
+++ total_sales2011.sql
@@ -1,2 +1,6 @@
 -- What are the respective total sales for each of those years?
 -- HINT: SUM
+
+SELECT SUM(Total) as Total_Sales_2011
+FROM Invoice
+WHERE InvoiceDate Like "%2011%";
